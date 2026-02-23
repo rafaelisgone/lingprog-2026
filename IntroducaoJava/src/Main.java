1492b2b99c6562bc739918a2336624043c76fc54
RAFAEL SEIXAS CINTRA <rafaelseixascintra@gmail.com>
2026-02-23 10:38:41 -0300

    imc

---
+++ IntroducaoJava/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        //variavel do tipo String
+        String nome = "Rafael";
+
+        //variavel do tipo inteiro
+        int idade = 18;
+
+        //variavel do tipo float
+        float peso = 92.5f;
+        float altura = 1.53f;
+
+        //calcular o imc
+        float imc = peso / (altura * altura);
+
+        System.out.println(nome + " seu IMC é: " + imc);
+    }
+}
