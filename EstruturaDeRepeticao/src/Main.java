d9f47319f629fb0b4a0c05cd37919686b8556953
unknown <rafaelseixascintra@gmail.com>
2026-03-10 07:25:10 -0300

    EstruturaDeRepeticao

---
+++ EstruturaDeRepeticao/src/Main.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+        /*int condicao = 1;
+        while (condicao <= 10){
+            System.out.println("Oi exibido quando condicao = " + condicao);
+            System.out.println((int) (Math.random()*10) );
+            condicao++;
+        }
+
+
+        int condicao = 1;
+        float nota = 0;
+        float soma = 0;
+        while (condicao <= 10){
+            System.out.println("Informe uma nota");
+            nota = entrada.nextInt();
+            if (nota < 0 || nota > 10){ // || é o OR
+                break; // força a saída do while
+            }
+            soma += nota;
+            condicao++;
+        } // fecha o while
+        if (condicao == 11){ // foi normal, sem entrar no break
+            float media = soma / 10;
+            System.out.println("A média é " + media);
+        }
+        else {
+            System.out.println("Nota inválida ");
+        } */
+
+        int condicao = 1;
+        float nota = 0;
+        float soma = 0;
+        while (condicao <= 5){
+            System.out.println("Informe uma nota");
+            nota = entrada.nextInt();
+            if (nota < 0 || nota > 10){
+                System.out.println("Nota inválida, tente novamente");
+                continue; // não sai do loop, ignora o que tem embaixo
+            }
+            condicao++;
+            soma += nota;
+        }
+        float media = soma / 5;
+        System.out.println("Média é " + media);
+    }
+}
---
+++ Exercicios/src/Main.java
@@ -0,0 +1,56 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("------------------------------------------------");
+        System.out.println("================= JAVA LANCHES =================");
+        System.out.println("------------------------------------------------");
+
+        // LEITURA DO PEDIDO
+        System.out.println("Digite o código do produto desejado:");
+        System.out.println("CÓDIGO 101: CACHORRO QUENTE (R$, 10,00).");
+        System.out.println("CÓDIGO 102: X-BURGUER (R$, 15,00).");
+        System.out.println("CÓDIGO 103: X-SALADA (R$, 12,00).");
+        int codigo = entrada.nextInt();
+        double valor = 0;
+
+        switch (codigo) {
+            case 101:
+                valor = 10;
+                break;
+
+            case 102:
+                valor = 15;
+                break;
+
+            case 103:
+                valor = 12;
+                break;
+
+            default:
+                System.out.println("Código inválido.");
+        }
+
+        //DESCONTOS
+        System.out.println("Digite sua idade:" +
+                "\nOBS: A depender da idade, seu pedido terá um desconto.");
+        int idade = entrada.nextInt();
+
+        if (idade < 12){
+            valor = valor / 2;
+        }
+        else if (idade >= 12 && idade <= 60){
+            valor = valor;
+        } else if (idade > 60) {
+            valor = valor - (valor * 0.3);
+        }
+
+        //FINAL
+        System.out.printf("Seu pedido foi concluído. Total da compra: R$ %.2f", valor);
+    }
+}
