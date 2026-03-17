a7d5d83c3a4bc5db6f1316be9752e8c8897f7c71
RAFAEL SEIXAS CINTRA <1090482521016@fatec-srv.local>
2026-03-17 10:22:16 -0300

    Rework

--- IntroducaoJava/src/Main.java
+++ IntroducaoJava/src/Main.java
@@ -1,21 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
 
-        //INTRODUÇÃO - ANOTAÇÕES
-
-        //variavel do tipo String
-        String nome = "Rafael";
-
-        //variavel do tipo inteiro
-        int idade = 18;
-
-        //variavel do tipo float
-        float peso = 92.5f;
-        float altura = 1.53f;
-
-        //calcular o imc
-        float imc = peso / (altura * altura);
-
-        System.out.println(nome + " seu IMC é: " + imc + ".");
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
     }
 }
