9bb25e969c9d979ade8b2738b2041f542bb58279
RAFAEL SEIXAS CINTRA <1090482521016@fatec-srv.local>
2026-05-05 10:58:37 -0300

    Delete e consulta

--- Loja/src/Main.java
+++ Loja/src/Main.java
@@ -1,43 +1,83 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.sql.*;
 
 public class Main{
+
+    // cria tabelas
     public static void criarTabela(Connection conn) throws SQLException{
         String sql = "CREATE TABLE IF NOT EXISTS produtos (" +
                 "id SERIAL PRIMARY KEY, " +
                 "nome TEXT NOT NULL, " +
                 "preco REAL NOT NULL, " +
                 "estoque INTEGER DEFAULT 0)";
-
-        //cria objeto de instrução SQL
+        // cria objeto de instrução SQL
         Statement stmt = conn.createStatement();
-        stmt.execute(sql); // executa comando sql
-        stmt.close(); // fecha instrução sql
+        stmt.execute(sql); // executa comando SQL
+        stmt.close(); // fecha instrução SQL
+        System.out.println("Tabela produtos criada com sucesso");
     }
-    public static void insere(Connection conn, String nome, double preco, int estoque) throws SQLException {
-        // cria sql
-        String sql = "INSERT INFO produtos (nome, preco, estoque) value (?, ?, ?)";
+
+    // deleta um registro
+    public static void removerProduto(Connection conn, int id) throws SQLException{
+        String sql = "DELETE FROM  produtos WHERE id = ?";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setInt(1, id);
+        int linhasAfetadas = ps.executeUpdate();
+        ps.close();
+        if (linhasAfetadas > 0){
+            System.out.println("Produto removido!");
+        }
+        else System.out.println("ID não encontrado.");
+
+    }
+
+    // consulta
+    public static void consulta(Connection conn) throws SQLException{
+        // criar comando sql
+        String sql = "SELECT * FROM produtos ORDER BY nome";
+        Statement stat = conn.createStatement();
+        // executa a consulta no banco e armazena o resultado em rs
+        ResultSet rs = stat.executeQuery(sql);
+        while(rs.next()){
+            // recupera o valor de cada coluna
+            int id = rs.getInt("id");
+            String nome = rs.getString("nome");
+            double preco = rs.getDouble("preco");
+            int estoque = rs.getInt("estoque");
+            // mostra o registro da consulta
+            System.out.printf("[%d] %s - R$ %.2f (estoque: %d)%n", id, nome, preco, estoque);
+        }
+    }
+
+    // insere
+    public static void insere(Connection conn, String nome, double preco, int estoque)
+            throws SQLException {
+        // cria SQL
+        String sql = "INSERT INTO produtos (nome, preco, estoque) values (?, ?, ?)";
         // prepara uma instrução SQL
         PreparedStatement ps = conn.prepareStatement(sql);
         ps.setString(1, nome); // define o nome do produto
-        ps.setDouble(2, preco); // define o preco do produto
+        ps.setDouble(2, preco); // define o preço do produto
         ps.setInt(3, estoque); // define estoque do produto
-        ps.executeUpdate(); //executa o SQL
-        System.out.print("Produto inserido com sucesso");
+        ps.executeUpdate(); // executa o SQL
+        System.out.println("Produto inserido com sucesso");
         ps.close();
     }
-        public static void main(String[] args){
+
+    public static void main(String[] args){
         String url = "jdbc:postgresql://localhost:5432/loja";
-        try{ //tenta se conectar no BD
-            Connection conn = DriverManager.getConnection(url, "postgres", "fatec123*");
-            System.out.print("Conexão com sucesso.");
+        try { // tenta se conectar no banco de dados
+            Connection conn = DriverManager.getConnection(
+                    url, "postgres", "fatec123*");
+            System.out.println("Conexão com sucesso");
+            // cria a tabela produto
             criarTabela(conn);
-            insere(conn, "mouse", 149, 3);
+            // insere(conn, "mouse", 45, 5);
+            // consulta(conn);
+            // removerProduto(conn, 1);
+
         }
-        catch (SQLException e){
-            System.out.print("Erro ao se conectar com o banco.\n"+ e.getMessage());
+        catch (SQLException e){ // caso dê erro, desvia pra cá
+            System.out.println("Erro ao conectar com o banco " + e.getMessage());
         }
     }
 }
