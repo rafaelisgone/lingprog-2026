089e436b3922cdc8283ea1f4643ce57daf61c21d
RAFAEL SEIXAS CINTRA <1090482521016@fatec-srv.local>
2026-05-18 10:54:19 -0300

    Atividade em andamento

--- ExBiblioteca/src/Main.java
+++ ExBiblioteca/src/Main.java
@@ -3,8 +3,41 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.*;
+import java.util.Scanner;
 
 public class Main{
+    // menu
+    public static void exibirMenu (){
+        System.out.println("========== Biblioteca  ==========");
+        System.out.print("1. Listar livros.\n" +
+                "2. Cadastrar livro.\n" +
+                "3. Emprestar livro.\n" +
+                "4. Devolver livro.\n" +
+                "5. Remover livro.\n" +
+                "0. Sair.\n" +
+                "Opção: ");
+    }
+
+    // processar opcao
+    public static void processarOpcao (Connection conn, Scanner sc, int opcao) throws SQLException {
+        switch (opcao){
+            case 1: listarLivros(conn); break;
+            case 2: cadastrarLivros(conn, sc); break;
+            case 3: emprestarLivros(conn, sc); break;
+            case 4: devolverLivros; break;
+            case 5: removerLivros; break;
+            case 0:
+                System.out.println("Encerrando..."); break;
+
+            default:
+                System.out.println("Opção inválida.");
+        }
+    }
+
+    //==================================================================
+
+    // funções do CRUD
+    // cria tabela
     public static void criarTabela(Connection conn) throws SQLException{
         String sql = "CREATE TABLE IF NOT EXISTS livros (" +
                 "id SERIAL PRIMARY KEY, " +
@@ -18,9 +51,45 @@ public class Main{
         stmt.close(); // fecha instrução SQL
     }
 
+    // cadastrar livros
+    public static void cadastrarLivros(Connection conn, Scanner sc)
+            throws SQLException {
+        System.out.println("Digite o título do livro:");
+        String titulo = sc.nextLine();
+        System.out.println("Digite o autor do livro:");
+        String autor = sc.nextLine();
+        System.out.println("Digite o ano do lançamento do livro:");
+        int ano = sc.nextInt();
+        int dispo = 1;
 
+        String sql = "INSERT INTO livros (titulo, autor, ano, dispo) values (?, ?, ?, ?)"; // cria SQL
+        PreparedStatement ps = conn.prepareStatement(sql); // prepara uma instrução SQL
+        ps.setString(1, titulo); // define o titulo do livro
+        ps.setString(2, autor); // define o autor do livro
+        ps.setInt(3, ano); // define o ano do livro
+        ps.setInt(4, dispo); // define a disponibilidade
+        ps.executeUpdate(); // executa o SQL
+        System.out.println("Livro cadastrado com êxito!");
+        ps.close();
+    }
 
-    //======================================================================================
+    // lista de livros
+    public static void listarLivros (Connection conn) throws SQLException{
+        String sql = "SELECT * FROM livros ORDER BY titulo"; // criar comando sql
+        Statement stat = conn.createStatement();
+        ResultSet rs = stat.executeQuery(sql); // executa a consulta no banco e armazena o resultado
+        while(rs.next()){
+            // recupera o valor de cada coluna
+            int id = rs.getInt("id");
+            String titulo = rs.getString("titulo");
+            String autor = rs.getString("autor");
+            int ano = rs.getInt("ano");
+            int dispo = rs.getString("disponivel");
+            // mostra o registro da consulta
+
+            System.out.printf("", id, titulo, autor, ano, dispo);
+        }
+    }
 
     public static void main (String[] args){
         String url = "jdbc:postgresql://localhost:5432/biblioteca";
@@ -30,6 +99,14 @@ public class Main{
             System.out.print("Conexão com sucesso");
             // conexão com o banco
             criarTabela(conn);
+            Scanner sc = new Scanner(System.in);
+            int opcao = 1;
+            do {
+                exibirMenu();
+                opcao = sc.nextInt();
+                sc.nextLine();
+                processarOpcao(conn, sc, opcao);
+            } while (opcao != 0);
         }
         catch (SQLException e){ // caso dê erro, desvia pra cá
             System.out.print("Erro ao conectar com o banco " + e.getMessage());
